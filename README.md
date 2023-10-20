2093a5ddc9ec055fa32b67d0ce84cd0586e47959
Alexis Le Glaunec <alexisdu51151@gmail.com>
2023-10-20 10:18:56 -0500

    references added

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ Implementation of Linear Algebra Subroutines to learn more about the algorithms 
 
 ## Algorithms implemented:
 - SAXPY (C, C++ and CUDA)
+
+
+## References:
+- [BLAS](https://www.netlib.org/blas/)]
+- [cuBLAS](https://docs.nvidia.com/cuda/cublas/index.html)
+- [cuBLAS API Examples](https://github.com/NVIDIA/CUDALibrarySamples/tree/master/cuBLAS)
--- utils.h
+++ utils.h
@@ -1,8 +1,28 @@
-//
-// Created by alg on 10/19/23.
-//
+// Taken from https://github.com/NVIDIA/CUDALibrarySamples/blob/master/cuBLAS/utils/cublas_utils.h
 
 #ifndef CUDA_BLAS_UTILS_H
 #define CUDA_BLAS_UTILS_H
+
+// CUDA API error checking
+#define CUDA_CHECK(err)                                                                            \
+    do {                                                                                           \
+        cudaError_t err_ = (err);                                                                  \
+        if (err_ != cudaSuccess) {                                                                 \
+            std::printf("CUDA error %d at %s:%d\n", err_, __FILE__, __LINE__);                     \
+            throw std::runtime_error("CUDA error");                                                \
+        }                                                                                          \
+    } while (0)
+
+// cublas API error checking
+#define CUBLAS_CHECK(err)                                                                          \
+    do {                                                                                           \
+        cublasStatus_t err_ = (err);                                                               \
+        if (err_ != CUBLAS_STATUS_SUCCESS) {                                                       \
+            std::printf("cublas error %d at %s:%d\n", err_, __FILE__, __LINE__);                   \
+            throw std::runtime_error("cublas error");                                              \
+        }                                                                                          \
+    } while (0)
+    
+    
 
 #endif //CUDA_BLAS_UTILS_H
